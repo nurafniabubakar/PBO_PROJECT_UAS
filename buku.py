b740cadac7c8121d402ac5258f952153435709c9
Nurafni Abubakar <nurafniabubakar227@gmail.com>
2025-12-20 06:40:30 +0900

    hanya fork bagian main saja atau menu

--- buku.py
+++
@@ -1,8 +0,0 @@
-class Buku:
-    def __init__(self, judul, penulis):
-        self.judul = judul
-        self.penulis = penulis
-
-    def info(self):
-        print(f"Judul   : {self.judul}")
-        print(f"Penulis : {self.penulis}")
